605348107717575236bd172c1a6eb5dea99e2fde
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-12-26 15:44:01 +0900

    検索フィルター機能を追加

--- reactApp/react-sample/src/App.js
+++ reactApp/react-sample/src/App.js
@@ -1,38 +1,64 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import { Suspense, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 
-import { Table } from './components/Table';
+import './App.css';
+import Table from './components/Table';
 
 axios.defaults.headers.get['Content-Type'] = 'application/json';
 axios.defaults.headers.get.Accept = 'application/json';
-axios.defaults.baseURL = 'http://localhost:3001/';
+axios.defaults.baseURL = 'http://0.0.0.0:3001/';
 
-const COLUMNS = ['id', 'name', 'age'];
+const COLUMNS = [
+  {
+    accessorKey: 'id',
+    header: 'ID',
+  },
+  {
+    accessorKey: 'name',
+    header: 'なまえ',
+  },
+  {
+    accessorKey: 'age',
+    header: '年齢',
+  },
+];
+
+const Pets = ({
+  values,
+  setState,
+}) => {
+
+  if (values === null) {
+    if (setState) {
+      throw axios.get('/cats').then(response => setState(response.data));
+    }
+  }
+
+  return (
+    <Table
+      bordered
+      hover
+      striped
+      variant="dark"
+      columns={COLUMNS}
+      rows={values}
+    />
+  );
+};
 
 function App() {
 
-  const [cats, setCats] = useState([]);
-
-  useEffect(() => {
-    axios.get('/cats').then(response => setCats(response.data));
-  }, []);
+  const [cats, setCats] = useState(null);
 
   return (
-    <>
-      <div className="bg-dark" style={{ minHeight: '100vh' }}>
-        <div className="p-5">
-          <Table
-            bordered
-            hover
-            striped
-            variant="dark"
-            columns={COLUMNS}
-            rows={cats.map(cat => [cat.id, cat.name, cat.age])}
-          />
-        </div>
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header p-5">
+        <Suspense fallback={<Spinner animation="border" variant="light" />}>
+          <Pets values={cats} setState={setCats} />
+        </Suspense>
+      </header>
+    </div>
   );
 }
 
--- reactApp/react-sample/src/components/Table.jsx
+++ reactApp/react-sample/src/components/Table.jsx
@@ -1,37 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table as BootstrapTable } from 'react-bootstrap';
+import { useReactTable, flexRender, getCoreRowModel, getFilteredRowModel, getSortedRowModel } from '@tanstack/react-table';
 
-export const Table = React.forwardRef(({
+const filter = (row, columnId, value) => {
+  return String(row.getValue(columnId)).indexOf(value) !== -1;
+}
+
+const DebouncedInput = ({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}) => {
+  const [value, setValue] = useState(initialValue)
+
+  useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => onChange(value), debounce);
+    return () => clearTimeout(timeout)
+  }, [value])
+
+  return (
+    <input {...props} value={value} onChange={e => setValue(e.target.value)} />
+  )
+}
+
+const Table = React.forwardRef(({
   columns,
   rows,
   ...otherProps
 }, ref) => {
 
+  const [globalFilter, setGlobalFilter] = useState('')
+
+  const table = useReactTable({
+    columns,
+    data: rows,
+    state: {
+      globalFilter,
+    },
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: filter,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  })
+
   return (
-    <BootstrapTable ref={ref} {...otherProps}>
-      {
-        columns && (
-          <thead>
-            <tr>
-              {columns.map((column, i) => <th key={i}>{column}</th>)}
-            </tr>
-          </thead>
-        )
-      }
-      {
-        rows && (
-          <tbody>
-            {
-              rows.map(row => (
-                <tr key={row[0]}>
-                  {row.map((column, i) => <td key={i}>{column}</td>)}
-                </tr>
-              ))
-            }
-          </tbody>
-        )
-      }
-    </BootstrapTable>
+    <div className="w-100">
+      <div className="mb-2 w-100" style={{ paddingLeft: 1, paddingRight: 1 }}>
+        <DebouncedInput
+          value={globalFilter ?? ''}
+          onChange={value => setGlobalFilter(String(value))}
+          className="w-100 p-2 font-lg shadow border border-block"
+          placeholder="検索"
+        />
+      </div>
+
+      <BootstrapTable ref={ref} {...otherProps}>
+        <thead>
+          {
+            table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {
+                  headerGroup.headers.map(header => {
+                    return (
+                      <th key={header.id}>
+                        {
+                          header.isPlaceholder
+                            ? null
+                            : flexRender(header.column.columnDef.header, header.getContext())
+                        }
+                      </th>
+                    )
+                  })
+                }
+              </tr>
+            ))
+          }
+        </thead>
+        <tbody>
+          {
+            table.getRowModel().rows.map(row => (
+              <tr key={row.id}>
+                {
+                  row.getVisibleCells().map(cell => (
+                    <td key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </tbody>
+      </BootstrapTable>
+    </div>
   )
 })
 
